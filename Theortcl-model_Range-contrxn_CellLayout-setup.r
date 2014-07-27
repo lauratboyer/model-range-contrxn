beac7843fbcdb49c66ab0de881ad6f8318cc50a6
lauratb <laura.t.boyer@gmail.com>
2014-07-27 10:35:35 -0700

    new source sink scenarios and figures plus started code for entire simulation in c++

---
+++ Theortcl-model_Range-contrxn_CellLayout-setup.r
@@ -0,0 +1,75 @@
+## Theortcl-model_Range-contrxn_CellLayout-setup.r
+## Theoretical model of range dynamics. Set-up cell layout for habitat quality.
+## -------------------------------------------------------
+## Author: Laura Tremblay-Boyer (l.boyer@fisheries.ubc.ca)
+## Written on: July 25, 2014
+## Time-stamp: <2014-07-25 11:39:04 Laura>
+
+
+## This one is for a middle core set-up
+## Calculate width of square in the middle
+## aiming between 30-40% of cells within core
+calc.core.size <- function(n=ncell) {
+
+    gw <- sqrt(n)
+    even.width <- (gw %% 2)==0
+    core.width <- round(sqrt(0.35*n))
+    even.core <-  (core.width %% 2)==0
+    # for core to be perfectly centered square should be
+    # even if range width is even and vice versa
+    if(!identical(even.width, even.core)) core.width <- core.width-1
+    edge.width <- 0.5*(gw - core.width)
+    core.start <- edge.width + 1
+    core.end <- core.start + core.width -1
+
+    mat1 <- matrix(0,nrow=gw,ncol=gw)
+    mat1[core.start:core.end,] <- 1
+    core.mat <- mat1*t(mat1)
+    core.ind <- which(core.mat==1)
+    edge.ind <- which(core.mat==0)
+
+    # calculate distance from center to set colours accordingly
+    center.val <- median(1:grid.width)
+    core.pos <- arrayInd(core.ind, .dim=c(grid.width, grid.width))
+    edge.pos <- arrayInd(edge.ind, .dim=c(grid.width, grid.width))
+
+    core.ind <- core.ind[order(-apply(abs(core.pos - center.val), 1, mean))]
+    edge.ind <- edge.ind[order(-apply(abs(edge.pos - center.val), 1, mean))]
+
+    return(list(core.mat=core.mat,core.cells=core.ind,edge.cells=edge.ind))
+}
+
+## Band core set-up (like latitudes)
+## Ideally want the same number of cells in core and bands,
+## if that is not possible find band combination that centers it in the middle
+## i.e. if grid.width is odd, center band + adjoining bands so long as # core < # edge
+## if even grid.width, two middle ones + adjoining...
+calc.core.band.size <- function(n=ncell) {
+
+    gw <- sqrt(n) # grid.width
+    even.width <- (gw %% 2)==0 # is grid.*width even
+    # round to half x nearest multiple of 4 to calculate core width
+    core.width <- 2*floor(gw/4) + ifelse(even.width, 0, 1)
+    # if grid.width is odd, keep core width smaller than edge width
+    if(core.width > 0.5*gw) core.width <- core.width - 2
+    edge.width <- gw - core.width
+
+    core.start <- 0.5*edge.width + 1
+    core.end <- core.start + core.width - 1
+
+    core.mat <- matrix(0,nrow=gw,ncol=gw)
+    core.mat[,core.start:core.end] <- 1
+    core.ind <- which(core.mat==1)
+    edge.ind <- which(core.mat==0)
+
+    # calculate distance from center to set colours accordingly
+    center.val <- median(1:grid.width)
+    core.pos <- arrayInd(core.ind, .dim=c(grid.width, grid.width))
+    edge.pos <- arrayInd(edge.ind, .dim=c(grid.width, grid.width))
+
+    core.ind <- core.ind[order(abs(core.pos[,2] - center.val))]
+    edge.ind <- edge.ind[order(abs(edge.pos[,2] - center.val))]
+
+    return(list(core.mat=core.mat,core.cells=core.ind,edge.cells=edge.ind))
+}
+
---
+++ Theortcl-model_Range-contrxn_Cpp-bloops.r
@@ -0,0 +1,133 @@
+## Theortcl-model_Range-contrxn_Cpp-bloops.r
+## Rewriting cpp version of model by running both loops in Cpp
+## -------------------------------------------------------
+## Author: Laura Tremblay-Boyer (l.boyer@fisheries.ubc.ca)
+## Written on: July 25, 2014
+## Time-stamp: <2014-07-25 20:30:10 Laura>
+
+require(Rcpp)
+
+cppFunction('NumericMatrix updcellcpp(NumericMatrix inipopmat,
+NumericVector rval_growth, NumericVector rval_mrt, NumericVector Kval,
+NumericVector emigv, NumericVector immigv,
+NumericVector fishmort) {
+int nr = inipopmat.nrow();
+int nc = inipopmat.ncol();
+NumericMatrix popmatnow(nr,nc); //empty matrix
+
+for (int cc = 0; cc < nc; cc++) {
+for (int rr = 0; rr < nr; rr++) {
+double Ntm1 = inipopmat[cc*nc+rr];
+// calculate cell abundance before fishing
+double Ntnow = Ntm1+rval_growth[cc*nc+rr]*Ntm1 -
+rval_mrt[cc*nc+rr]*Ntm1*Ntm1/Kval[cc*nc+rr] -
+emigv[cc*nc+rr]*Ntm1 + immigv[cc*nc+rr];
+// now remove proportion based on F
+popmatnow[cc*nc+rr] = Ntnow*(1-fishmort[cc*nc+rr]);
+}
+}
+return popmatnow; }')
+
+
+cppFunction('NumericMatrix updcellcpp_eb4r(NumericMatrix inipopmat,
+NumericVector rval_growth, NumericVector rval_mrt, NumericVector Kval,
+NumericVector emigv, NumericVector immigv,
+NumericVector fishmort) {
+int nr = inipopmat.nrow();
+int nc = inipopmat.ncol();
+NumericMatrix popmatnow(nr,nc); //empty matrix
+
+for (int cc = 0; cc < nc; cc++) {
+for (int rr = 0; rr < nr; rr++) {
+double Ntm1 = inipopmat[cc*nc+rr];
+// calculate cell abundance before fishing, emigration occurs before repr
+double Ntnow = Ntm1 - emigv[cc*nc+rr]*Ntm1 + immigv[cc*nc+rr];
+double Ntnowrep = Ntnow + rval_growth[cc*nc+rr]*Ntnow - rval_mrt[cc*nc+rr]*Ntnow*Ntnow/Kval[cc*nc+rr];
+// now remove proportion based on F
+popmatnow[cc*nc+rr] = Ntnowrep;
+}
+}
+return popmatnow; }')
+
+cppFunction('NumericMatrix ibcnkcpp(int ts,
+IntegerVector corecells, double rgrowth,
+NumericVector Kcell,
+NumericMatrix neighcell, NumericMatrix Nmat) {
+
+#include <math.h>
+
+int nr = Nmat.nrow();
+int nc = Nmat.ncol();
+int ncell = nr * nc; // number of cells
+NumericMatrix neighstore(ncell, 8);
+
+for (int wcell = 0; wcell < ncell; wcell++) {
+for (int neigh = 0; neigh < 8; neigh++) {
+
+// get neighbour cell id
+int neigh_id = neighcell[neigh*8 + wcell];
+neighstore[neigh*8 + wcell] = exp(1 - Nmat[neigh_id]/Kcell[neigh_id]);
+
+}
+}
+
+return neighstore; }')
+
+cell.dyn <- function(pref.disp=0, add.r=FALSE, use.home=FALSE, emig.before=TRUE) { # Nt as a function of N_t-1
+
+    # Reset population matrix
+    envpop$mat[,,2:ts.max] <<- NA
+
+    upd.ts <- function(ts) {
+
+        matnow <- envpop$mat[,,ts-1]
+        # first step: fish and chips
+        # using fishing mortality as defined in current timestep
+        fishmat <- matnow*(1-F.array[,,ts])
+        matnow <- fishmat
+
+        # then calculate proportion of each cell that emigrates given
+        # current, post-fishing N
+        prop.emig <- calc.emig.straight(matnow)
+
+        if(use.home){
+            pi <- ibc.nkratio.wnatal(ts, matnow, pref.disp=pref.disp, add.r=add.r)
+            prop.emig <- apply(pi, 1, sum) # emigration sum of relative proportions that go to neighbours
+
+        }
+
+        # store it
+        envpop$emig.mat[,,ts] <- prop.emig
+
+        # calculate emigration matrix for this time-step
+        # for each cell, N_t-1 * prop.emig
+        #emig.by.cell <- c(envpop$mat[,,ts-1]*prop.emig)
+        emig.by.cell <- c(matnow*prop.emig)
+
+
+        # assign to each neighbour by multiplying on neighbour mat
+        # and spreading evenly over neighbours by dividing by # neighbours
+        # senders in rows, receivers in columns
+        prop.immig <- ibc.nkratio(ts, matnow, pref.disp=pref.disp, add.r=add.r)
+        envpop$immig.rate[,,ts] <- prop.immig
+        immig.by.cell <- apply(emig.by.cell*prop.immig, 2, sum)
+        envpop$immig.store[ts,] <- immig.by.cell
+        envpop$emig.store[ts,] <- emig.by.cell
+
+        if(emig.before) {
+        matupd <- updcellcpp_eb4r(matnow, r.growth, r.mrt, K,
+                             prop.emig, immig.by.cell, c(F.array[,,ts]))
+        }else{
+        matupd <- updcellcpp(matnow, r.growth, r.mrt, K,
+                             prop.emig, immig.by.cell, c(F.array[,,ts]))
+    }
+
+        # update value stored in population array
+        envpop$mat[,,ts] <- matupd
+
+
+    }
+
+    dmm <- sapply(2:ts.max, upd.ts)
+}
+
